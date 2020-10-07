19aa06b2ab507189c4ca135fcdd398e41418264d
NoamDahan <noamdahan12@gmail.com>
2020-10-07 21:11:22 +0300

    hi

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.invites = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,22 +42,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // invites
             // 
-            this.button2.Location = new System.Drawing.Point(344, 182);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 86);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "הזמנות";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.invites.Location = new System.Drawing.Point(344, 182);
+            this.invites.Name = "invites";
+            this.invites.Size = new System.Drawing.Size(112, 86);
+            this.invites.TabIndex = 1;
+            this.invites.Text = "הזמנות";
+            this.invites.UseVisualStyleBackColor = true;
+            this.invites.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.invites);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,7 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button invites;
     }
 }
 
